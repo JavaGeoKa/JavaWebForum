6cab5481178f38c79ba5227b80c5315e8896d990
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-09-05 08:56:58 +0300

    Forum

---
+++ 66_ForumServiceSpringSecurity/src/main/java/telran/ashkelon2020/accounting/security/configuration/SecurityAuthorizationConfiguration.java
@@ -0,0 +1,42 @@
+package telran.ashkelon2020.accounting.security.configuration;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityAuthorizationConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring().antMatchers("/account/register");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET).permitAll()
+			.antMatchers(HttpMethod.POST, "/forum/posts/**").permitAll()
+			.antMatchers("/account/user/{login}/role/{role}**")
+				.hasRole("ADMINISTRATOR")
+			.antMatchers("/account/login**", "/forum/post/{id}/like**")
+				.hasAnyRole("ADMINISTRATOR", "MODERATOR", "USER")
+			.antMatchers("/account/user/{login}**")
+				.access("#login==authentication.name")
+			.antMatchers(HttpMethod.PUT,"/forum/post/{id}**")
+				.access("@customSecurity.checkPostAuthority(#id, authentication.name) or hasRole('MODERATOR')")
+			.antMatchers("/account/password**")
+				.authenticated()
+			.anyRequest()
+				.authenticated();
+		
+	}
+
+}
